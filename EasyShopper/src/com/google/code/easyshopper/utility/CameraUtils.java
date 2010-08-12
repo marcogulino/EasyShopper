cdd2bc3dc7a001f474e419e1c73488946689050d
Marco Gulino <marco.gulino@gmail.com>
2010-08-12 17:40:53 +0200

    Get external storage directory by environment

--- EasyShopper/src/com/google/code/easyshopper/utility/CameraUtils.java
+++ EasyShopper/src/com/google/code/easyshopper/utility/CameraUtils.java
@@ -1,43 +1,42 @@
 package com.google.code.easyshopper.utility;
 
 import java.io.File;
-import java.util.ArrayList;
 
 import android.net.Uri;
+import android.os.Environment;
 
 import com.google.code.easyshopper.ES;
-import com.google.code.easyshopper.Logger;
 
 public class CameraUtils {
-	public static final String SAVED_PATH = "/sdcard/Android/data/" + ES.class.getPackage().getName();
+	public static final String SAVED_PATH = Environment.getExternalStorageDirectory() + "/Android/data/" + ES.class.getPackage().getName();
 
-	public static boolean hasImageCaptureBug() {
-
-	    // list of known devices that have the bug
-	    ArrayList<String> devices = new ArrayList<String>();
-	    devices.add("android-devphone1/dream_devphone/dream");
-	    devices.add("generic/sdk/generic");
-	    devices.add("vodafone/vfpioneer/sapphire");
-	    devices.add("tmobile/kila/dream");
-	    devices.add("verizon/voles/sholes");
-	    devices.add("tmobile/opal/sapphire");
-	    devices.add("google_ion/google_ion/sapphire");
-	    devices.add("htc_wwe/htc_bravo/bravo");
-
-	    String myDevice = android.os.Build.BRAND + "/" + android.os.Build.PRODUCT + "/"
-	            + android.os.Build.DEVICE; // + "/" + android.os.Build.VERSION.SDK_INT;
-	    boolean hasBug = devices.contains(myDevice);
-	    Logger.dbg(CameraUtils.class, "hasImageCaptureBug", "*** checking camera bug for device: " + myDevice + "=" + hasBug);
-		return hasBug;
-	}
+//	public static boolean hasImageCaptureBug() {
+//
+//	    // list of known devices that have the bug
+//	    ArrayList<String> devices = new ArrayList<String>();
+//	    devices.add("android-devphone1/dream_devphone/dream");
+//	    devices.add("generic/sdk/generic");
+//	    devices.add("vodafone/vfpioneer/sapphire");
+//	    devices.add("tmobile/kila/dream");
+//	    devices.add("verizon/voles/sholes");
+//	    devices.add("tmobile/opal/sapphire");
+//	    devices.add("google_ion/google_ion/sapphire");
+//	    devices.add("htc_wwe/htc_bravo/bravo");
+//
+//	    String myDevice = android.os.Build.BRAND + "/" + android.os.Build.PRODUCT + "/"
+//	            + android.os.Build.DEVICE; // + "/" + android.os.Build.VERSION.SDK_INT;
+//	    boolean hasBug = devices.contains(myDevice);
+//	    Logger.dbg(CameraUtils.class, "hasImageCaptureBug", "*** checking camera bug for device: " + myDevice + "=" + hasBug);
+//		return hasBug;
+//	}
 	
 	public static Uri getImagePath(String path) {
 		Uri uri;
-		if (hasImageCaptureBug()) {
+//		if (hasImageCaptureBug() || true) {
 			uri = Uri.fromFile(new File(SAVED_PATH));
-		} else {
-		 uri=android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-		}
+//		} else {
+//		 uri=android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//		}
 		return uri.buildUpon().appendPath( path).build();
 	}
 	
